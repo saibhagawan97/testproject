063a689f64d92614afad53be6845378576f6e9cf
saibhagawan97 <saibhagawan2222@gmail.com>
2021-10-27 11:57:43 +0530

    3rd one
    Signed-off-by: saibhagawan97 <saibhagawan2222@gmail.com>

--- testpro/src/testpro/testj.java
+++ testpro/src/testpro/testj.java
@@ -4,7 +4,7 @@ public class testj {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("new testproject");
+		 System.out.println("new testproject");
 	}
 
 }
