9adb8e5b4a1244300acf153b1a905fab8a3926b6
saibhagawan97 <saibhagawan2222@gmail.com>
2021-10-27 12:23:24 +0530

    6th one testing merge

--- testpro/src/testpro/testj.java
+++ testpro/src/testpro/testj.java
@@ -6,6 +6,7 @@ public class testj {
 		// TODO Auto-generated method stub
 		 	System.out.println("new testproject");
 		 System.out.println("checking for token working");
+		 System.out.println("checking merge");
 	}
 
 }
